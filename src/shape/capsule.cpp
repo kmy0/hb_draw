da67b79869cfe91ae06dbbeee088805d66169cc8
kmy0 <93156259+kmy0@users.noreply.github.com>
2025-09-19 10:22:44 +0200

    refactor capsule

--- src/shape/capsule.cpp
+++ src/shape/capsule.cpp
@@ -25,25 +25,26 @@ Capsule::Capsule(const Vector3f &start, const Vector3f &end, float radius) {
         return;
     }
 
-    const auto r_diff = m_bottom.radius - m_top.radius;
-    const auto t_length_bottom = std::sqrt(
-        std::max(0.0f, m_bottom.radius * m_bottom.radius -
-                           (r_diff * r_diff) / (m_distance * m_distance) *
-                               m_bottom.radius * m_bottom.radius));
-    const auto t_length_top = std::sqrt(
-        std::max(0.0f, m_top.radius * m_top.radius -
-                           (r_diff * r_diff) / (m_distance * m_distance) *
-                               m_top.radius * m_top.radius));
-    const auto dir = ctcb / m_distance;
-    const auto perp = Vector2f(-dir.y, dir.x);
-    const auto h = r_diff / m_distance;
+    const float r_diff = m_bottom.radius - m_top.radius;
+    const float inv_distance = 1.0f / m_distance;
+    const Vector2f dir = ctcb * inv_distance;
+    const Vector2f perp(-dir.y, dir.x);
+    const float h = r_diff * inv_distance;
+    const float sqrt_factor = std::sqrt(std::max(
+        0.0f, 1.0f - (r_diff * r_diff) * (inv_distance * inv_distance)));
 
-    m_quad[0] = m_top.center + dir * (h * m_top.radius) + perp * t_length_top;
-    m_quad[1] = m_top.center + dir * (h * m_top.radius) - perp * t_length_top;
-    m_quad[2] =
-        m_bottom.center + dir * (h * m_bottom.radius) - perp * t_length_bottom;
-    m_quad[3] =
-        m_bottom.center + dir * (h * m_bottom.radius) + perp * t_length_bottom;
+    const float t_length_bottom = m_bottom.radius * sqrt_factor;
+    const float t_length_top = m_top.radius * sqrt_factor;
+
+    const Vector2f top_base = m_top.center + dir * (h * m_top.radius);
+    const Vector2f bottom_base = m_bottom.center + dir * (h * m_bottom.radius);
+    const Vector2f perp_top = perp * t_length_top;
+    const Vector2f perp_bottom = perp * t_length_bottom;
+
+    m_quad[0] = top_base + perp_top;
+    m_quad[1] = top_base - perp_top;
+    m_quad[2] = bottom_base - perp_bottom;
+    m_quad[3] = bottom_base + perp_bottom;
 
     for (const auto &p : m_quad) {
         if (!is_point_ok(p)) {
@@ -56,10 +57,12 @@ Capsule::Capsule(const Vector3f &start, const Vector3f &end, float radius) {
     m_bottom.a_min = get_angle(m_quad[2], m_bottom.center);
     m_bottom.a_max = get_angle(m_quad[3], m_bottom.center);
 
-    m_top.a_min = m_top.a_min < m_top.a_max ? m_top.a_min + glm::radians(360.0f)
-                                            : m_top.a_min;
-    m_bottom.a_min = m_bottom.a_min < m_bottom.a_max
-                         ? m_bottom.a_min + glm::radians(360.0f)
-                         : m_bottom.a_min;
+    constexpr float TWO_PI = glm::radians(360.0f);
+    if (m_top.a_min < m_top.a_max) {
+        m_top.a_min += TWO_PI;
+    }
+    if (m_bottom.a_min < m_bottom.a_max) {
+        m_bottom.a_min += TWO_PI;
+    }
     m_is_ok = true;
 }
